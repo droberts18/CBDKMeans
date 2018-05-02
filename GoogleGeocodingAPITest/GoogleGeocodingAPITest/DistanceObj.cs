a08ac3dd5449c093c0213110e8dd2833bc6bf84f
droberts18 <droberts18@LAB-SCI-304-10>
2018-05-01 18:23:08 -0700

    Add program and package

---
+++ GoogleGeocodingAPITest/GoogleGeocodingAPITest/DistanceObj.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace GoogleGeocodingAPITest
+{
+    class DistanceObj
+    {
+        public string address { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public double[] coords { get; set; }
+
+        // used for creating home location
+        public DistanceObj(string address)
+        {
+            this.address = address;
+            retrieveLatAndLong();
+            // origin
+            coords = new double[] { 0, 0 };
+        }
+
+        // used for creating destinations
+        public DistanceObj(string address, double homeLong, double homeLat)
+        {
+            this.address = address;
+            retrieveLatAndLong();
+            coords = new double[2];
+            calculateCoords(homeLong, homeLat);
+        }
+
+        private void retrieveLatAndLong()
+        {
+            try
+            {
+                string geocodingApiUrl = ConfigurationManager.AppSettings["GoogleGeocodingApi"];
+                string apiKey = ConfigurationManager.AppSettings["ApiKey"];
+
+                string addressUrl = formatForUrl(address);
+
+                geocodingApiUrl += addressUrl + "&key=" + apiKey;
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(geocodingApiUrl);
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader sreader = new StreamReader(dataStream);
+                string responseReader = sreader.ReadToEnd();
+                response.Close();
+                DataSet ds = new DataSet();
+                ds.ReadXml(new XmlTextReader(new StringReader(responseReader)));
+
+                latitude = Convert.ToDouble(ds.Tables["location"].Rows[0].ItemArray[0]);
+                longitude = Convert.ToDouble(ds.Tables["location"].Rows[0].ItemArray[1]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error calculating location's latitude & longitude: " + ex.Message);
+            }
+        }
+
+        private string formatForUrl(string rawAddress)
+        {
+            string urlAddress = rawAddress.Replace(" ", "+");
+            return urlAddress;
+        }
+
+        public void calculateCoords(double homeLong, double homeLat)
+        {
+            // Dx - Wx
+            double xDist = longitude - homeLong;
+
+            //Dy - Wy
+            double yDist = latitude - homeLat;
+
+            // Calculating hypotenuse of triangle
+            // Square root of xDist^2 + yDist^2
+            double hypotenuse = Math.Sqrt(Math.Pow(xDist, 2) + Math.Pow(yDist, 2));
+
+            // Calculating xCoord
+            coords[0] = xDist / hypotenuse;
+
+            // Calculating yCoord
+            coords[1] = yDist / hypotenuse;
+        }
+
+        public void outputCoords()
+        {
+            Console.WriteLine("(" + coords[0] + ", " + coords[1] + ")\n");
+        }
+    }
+}
---
+++ GoogleGeocodingAPITest/GoogleGeocodingAPITest/Program.cs
@@ -0,0 +1,103 @@
+﻿using Accord.MachineLearning;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace GoogleGeocodingAPITest
+{
+    class Program
+    {
+        static string formatForUrl(string location)
+        {
+            location = location.Replace(" ", "+");
+            return location;
+        }
+
+        static void runKMeans(DistanceObj[] groups)
+        {
+            int numGroups = groups.Length;
+            // Declaring and intializing array for K-Means
+            double[][] observations = new double[numGroups][];
+
+            for (int i = 0; i < observations.Length; i++)
+            {
+                observations[i] = new double[2];
+                observations[i][0] = groups[i].coords[0];
+                observations[i][1] = groups[i].coords[1];
+            }
+
+            KMeans km = new KMeans(7);
+
+            KMeansClusterCollection clusters = km.Learn(observations);
+
+            int[] labels = clusters.Decide(observations);
+
+            for (int i = 0; i < labels.Length; i++)
+            {
+                Console.WriteLine(groups[i].address + ": " + labels[i]);
+            }
+        }
+
+        static void outputCsv(DistanceObj[] groups)
+        {
+            string filePath = @"C:\Users\droberts18\Desktop\DisIsForYouRyan.csv";
+            string delimiter = ",";
+
+            StringBuilder sb = new StringBuilder();
+            List<string> CsvRow = new List<string>();
+
+            CsvRow.Add("Address");
+            CsvRow.Add("X-Coordinate Relative to WU");
+            CsvRow.Add("Y-Coordinate Relative to WU");
+            sb.AppendLine(string.Join(delimiter, CsvRow));
+            CsvRow.Clear();
+
+            foreach(DistanceObj g in groups)
+            {
+                CsvRow.Add(g.address);
+                CsvRow.Add(g.coords[0].ToString().Replace(",", " "));
+                CsvRow.Add(g.coords[1].ToString().Replace(",", " "));
+                sb.AppendLine(string.Join(delimiter, CsvRow));
+                CsvRow.Clear();
+            }
+
+            File.WriteAllText(filePath, sb.ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            // Creating Whitworth as home
+            DistanceObj home = new DistanceObj("300 W. Hawthorne Rd., Spokane, WA");
+
+            // Reading in address and creating destination objects
+            string filename = @"C:\Users\droberts18\Desktop\CBDExampleAddresses.csv";
+            // Getting number of groups by reading number of lines in csv file
+            var numGroups = File.ReadLines(filename).Count();
+
+            DistanceObj[] groups = new DistanceObj[numGroups];
+
+            using (var reader = new StreamReader(filename))
+            {
+                int currentGroupsIndex = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    string curAddress = line.Replace("\"", "");
+                    groups[currentGroupsIndex] = new DistanceObj(curAddress, home.longitude, home.latitude);
+                    currentGroupsIndex++;
+                }
+            }
+
+            outputCsv(groups);
+        }
+    }
+}
